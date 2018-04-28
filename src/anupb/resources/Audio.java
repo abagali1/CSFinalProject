f19f175cec7a3f93422b3c03dd9086fa78a1635c
abagali1 <a.bagali815703@gmail.com>
2018-04-27 23:23:13 -0400

    Resources

--- src/anupb/resources/Audio.java
+++ src/anupb/resources/Audio.java
@@ -1,38 +1,47 @@
 package anupb.resources;
 
 import javax.sound.sampled.*;
-import java.io.File;
 
-public class Audio {
+public class Audio{
 
-    private File audiofile;
-    private AudioInputStream stream;
-    private AudioFormat format;
-    private DataLine.Info info;
     private Clip clip;
 
-    public Audio(String filename) throws Exception{
-        audiofile = new File(filename);
-        stream = AudioSystem.getAudioInputStream(audiofile);
-        format = stream.getFormat();
-        info = new DataLine.Info(Clip.class, format);
-        clip = (Clip) AudioSystem.getLine(info);
+    public Audio(String s) {
+
+        try {
+
+            AudioInputStream ais = AudioSystem.getAudioInputStream(getClass().getResource(s));
+            AudioFormat baseFormat = ais.getFormat();
+            AudioFormat decodeFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, baseFormat.getSampleRate(), 16,
+                    baseFormat.getChannels(), baseFormat.getChannels() * 2, baseFormat.getSampleRate(), false);
+            AudioInputStream dais = AudioSystem.getAudioInputStream(decodeFormat, ais);
+            clip = AudioSystem.getClip();
+            clip.open(dais);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
-    public void start(){
+    public void play() {
+        if (clip == null && clip.isRunning())
+            return;
+        stop();
+        clip.setFramePosition(0);
         clip.start();
     }
 
-    public void stop(){
-        clip.stop();
+    public void stop() {
+        if (clip.isRunning())
+            clip.stop();
     }
 
-    public Clip getClip(){
+    public void close() {
+        stop();
+        clip.close();
+    }
+
+    public Clip getClip() {
         return clip;
     }
-
-    public String getFile(){
-        return audiofile.getName();
-    }
-
 }
