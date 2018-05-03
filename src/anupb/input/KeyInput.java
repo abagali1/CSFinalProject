36356bc806934f1ecea39e4d7bdc1c0d7b52ac5a
abagali1 <a.bagali815703@gmail.com>
2018-05-02 22:13:41 -0400

    modified:   .idea/misc.xml 	modified:   .idea/workspace.xml 	renamed:    out/production/CSProject/anupb/panels/TetrisTheme.wav -> out/production/CSProject/anupb/audio/TetrisTheme.wav 	modified:   out/production/CSProject/anupb/driver/TetDriver.class 	modified:   out/production/CSProject/anupb/input/KeyInput.class 	modified:   out/production/CSProject/anupb/panels/BigPanel$1.class 	modified:   out/production/CSProject/anupb/panels/BigPanel$Starter.class 	modified:   out/production/CSProject/anupb/panels/BigPanel.class 	modified:   out/production/CSProject/anupb/panels/BlockPanel$1.class 	modified:   out/production/CSProject/anupb/panels/BlockPanel$Starter.class 	modified:   out/production/CSProject/anupb/panels/BlockPanel.class 	modified:   out/production/CSProject/anupb/panels/ScorePanel.class 	modified:   out/production/CSProject/anupb/panels/TetrisPanel$1.class 	modified:   out/production/CSProject/anupb/panels/TetrisPanel$Listener.class 	modified:   out/production/CSProject/anupb/panels/TetrisPanel.class 	modified:   out/production/CSProject/anupb/resources/Audio.class 	modified:   out/production/CSProject/anupb/resources/Block.class modified:   src/anupb/input/KeyInput.java 	modified:   src/anupb/panels/TetrisPanel.java

--- src/anupb/input/KeyInput.java
+++ src/anupb/input/KeyInput.java
@@ -34,11 +34,11 @@ public class KeyInput implements KeyListener {
      */
     @Override
     public void keyPressed(KeyEvent e) {
-        if(e.getKeyCode() == KeyEvent.VK_LEFT){
+        if(e.getKeyCode() == KeyEvent.VK_A){
             b.setLAcc(true);
             System.out.println("L");
         }
-        if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+        if(e.getKeyCode() == KeyEvent.VK_D){
             b.setRAcc(true);
             System.out.println("R");
         }
--- src/anupb/panels/TetrisPanel.java
+++ src/anupb/panels/TetrisPanel.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.util.Optional;
 
