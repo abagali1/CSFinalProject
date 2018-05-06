14319fd7887c74f330f869863c7dce1b49cd6742
abagali1 <a.bagali815703@gmail.com>
2018-05-06 13:38:58 -0400

    Bug Fixes

--- src/anupb/panels/leftPanel.java
+++ src/anupb/panels/leftPanel.java
@@ -3,6 +3,7 @@ package anupb.panels;
 import anupb.resources.Block;
 
 import javax.swing.*;
+import javax.swing.plaf.ComponentUI;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,6 +11,17 @@ import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Optional;
 
+/**
+ * A leftPanel is a panel used only for aesthetic purposes. A leftPanel holds information about its Graphical
+ * properties, along with 3 timers, an <code>ArrayList</code> of <code>Blocks</code>, and an array of possible y-positions
+ *
+ * @author Anup Bagali
+ * @author Kevin Liu
+ * @author Teja Kocherla
+ * @author Amit Rajesh
+ * @see Block
+ *
+ */
 public class leftPanel extends javax.swing.JPanel {
     private BufferedImage myImage;
     private Graphics2D myBuffer;
@@ -19,7 +31,9 @@ public class leftPanel extends javax.swing.JPanel {
     private Block temp;
     private int[] yPos = new int[] {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 0};
 
-
+    /**
+     * Creates a new leftPanel object
+     */
     public leftPanel(){
         myImage = new BufferedImage(200,401, BufferedImage.TYPE_INT_RGB);
         myBuffer = (Graphics2D) myImage.getGraphics();
@@ -36,10 +50,42 @@ public class leftPanel extends javax.swing.JPanel {
         t2.start();
     }
     @Override
+    /**
+     * Calls the UI delegate's paint method, if the UI delegate
+     * is non-<code>null</code>.  We pass the delegate a copy of the
+     * <code>Graphics</code> object to protect the rest of the
+     * paint code from irrevocable changes
+     * (for example, <code>Graphics.translate</code>).
+     * <p>
+     * If you override this in a subclass you should not make permanent
+     * changes to the passed in <code>Graphics</code>. For example, you
+     * should not alter the clip <code>Rectangle</code> or modify the
+     * transform. If you need to do these operations you may find it
+     * easier to create a new <code>Graphics</code> from the passed in
+     * <code>Graphics</code> and manipulate it. Further, if you do not
+     * invoke super's implementation you must honor the opaque property, that is
+     * if this component is opaque, you must completely fill in the background
+     * in an opaque color. If you do not honor the opaque property you
+     * will likely see visual artifacts.
+     * <p>
+     * The passed in <code>Graphics</code> object might
+     * have a transform other than the identify transform
+     * installed on it.  In this case, you might get
+     * unexpected results if you cumulatively apply
+     * another transform.
+     *
+     * @param g the <code>Graphics</code> object to protect
+     * @see #paint
+     * @see ComponentUI
+     */
     public void paintComponent(Graphics g){
         g.drawImage(myImage, 0, 0, getWidth(), getHeight(), null);
     }
 
+    /**
+     * A nested class for decorations
+     * @see ActionListener
+     */
     private class Listener implements ActionListener {
         /**
          * Invoked when an action occurs.
@@ -72,6 +118,10 @@ public class leftPanel extends javax.swing.JPanel {
         }
     }
 
+    /**
+     * A nested class for more decorations
+     * @see ActionListener
+     */
     private class Listener1 implements ActionListener {
         /**
          * Invoked when an action occurs.
@@ -103,6 +153,11 @@ public class leftPanel extends javax.swing.JPanel {
             revalidate();
         }
     }
+
+    /**
+     * A nested for even more decorations
+     * @see ActionListener
+     */
     private class Listener2 implements ActionListener {
         /**
          * Invoked when an action occurs.
