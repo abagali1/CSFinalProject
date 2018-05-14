a5fedcfab1a3fa6f0407eae30fe0abb2f9850cfc
abagali1 <35437096+abagali1@users.noreply.github.com>
2018-05-14 15:55:52 -0400

    Cleaned up flipping procedure, block getWidth()
    Added block getHeight() method

---
+++ Block.java
@@ -0,0 +1,526 @@
+package tetris.resources;
+
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.Optional;
+
+/**
+ * A Block is a combination of one or two rectangles to resemble a Tetrimino block. Two blocks only require one rectangle
+ * while the others require two rectangles. A block knows its x and y coordinate along with its width and height. If the block
+ * requires two rectangles then it also knows its second x and y coordinate along with its second width and height.
+ *
+ * @author Anup Bagali
+ * @author Teja Kocherla
+ * @author Kevin Liu
+ * @author Amit Rajesh
+ * @version 1.1
+ */
+public class Block {
+   /**
+    * Starting x-coordinate for all blocks
+    */
+   private int myX;
+   /**
+    * Starting y-coordinate for all blocks
+    */
+   private int myY;
+   /**
+    * Starting type for all blocks
+    * Types can only be from 0-6
+    */
+   private int myType;
+   /**
+    * Width for all blocks
+    * Width cannot be changed for any block
+    */
+   private int myWidth;
+   /**
+    * Height for all blocks
+    * Height cannot be changed for any block
+    */
+   private int myHeight;
+   /**
+    * Starting x-coordinate for blocks which require two rectangles
+    */    
+   private Integer myX2;
+   /**
+   * Retains the flip state of the block
+   */
+   private int myFlipState;
+   /**
+    * Starting y-coordinate for blocks which require two rectangles
+    */
+   private Integer myY2;
+   /**
+    * Width for blocks which require two rectangles
+    * Width cannot be changed for any block
+    */
+   private Integer myW2;
+   /**
+    * Height for blocks which require two rectangles
+    * Heigh cannot be changed for any block
+    */
+   private Integer myH2;
+   /**
+    * Color for all blocks
+    */
+   private Color myColor;
+   /**
+    * Determines whether block can move left
+    */
+   private boolean lacc;
+   /**
+    * Determines whether block can move right
+    */
+   private boolean racc;
+   /**
+    * Determines whether the rainfall motion should activate
+    */
+   private static boolean fall;
+   /**
+    * Determines whether a block is in the finished state
+    */
+   private boolean ifFinished;
+   /**
+    * Stores all the possible y positions for new blocks
+    */
+   private static int[] yPos = new int[] {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140,
+          150, 160, 170, 0};
+   /**
+    * Used to index <code>ArrayLists</code> of blocks
+    */
+   public static int count = 0;
+   /**
+    * Stores all the blocks in the finished state
+    */
+   public static ArrayList<Block> constantBlocks = new ArrayList<>();
+
+   /**
+    * Constructor for a block.
+    * @param x Starting x1 coordinate
+    * @param y Starting y1 coordinate
+    * @param x2 Optional parameter for x2 coordinate
+    * @param y2 Optional parameter for y2 coordinate
+    * @param t Type of tetrimino to be created
+    */
+   public Block(int x, int y, Optional<Integer> x2, Optional<Integer> y2, int t) {
+      this.myX = x;
+      myY = y;
+      myType = t;
+      myX2 = x2.isPresent() ? x2.get() : null;
+      myY2 = y2.isPresent() ? y2.get() : null;
+      myFlipState = 0;
+      if((myX2 == null || myY2 == null) && t > 1)
+         throw new NullPointerException();
+      switch (myType) {
+         case 0:
+            myWidth = 20;
+            myHeight = 20;
+            myW2 = null;
+            myH2 = null;
+            myColor = Color.yellow;
+            break;
+         case 1:
+            myHeight = 10;
+            myWidth = 40;
+            myW2 = null;
+            myH2 = null;
+            myColor = Color.cyan;
+            break;
+         case 2:
+            myWidth = 30;
+            myHeight = 10;
+            myX2 = myX + 20;
+            myY2 = myY + 10;
+            myW2 = 10;
+            myH2 = 10;
+            myColor = Color.orange;
+            break;
+         case 3:
+            myWidth = 30;
+            myHeight = 10;
+            myX2 = myX;
+            myY2 = myY + 10;
+            myW2 = 10;
+            myH2 = 10;
+            myColor = Color.blue;
+            break;
+         case 4:
+            myWidth = 20;
+            myHeight = 10;
+            myX2 = myX + 10;
+            myY2 = myY + 10;
+            myW2 = myWidth;
+            myH2 = myHeight;
+            myColor = Color.green;
+            break;
+         case 5:
+            myWidth = 20;
+            myHeight = 10;
+            myX2 = myX + 10;
+            myY2 = myY - 10;
+            myW2 = myWidth;
+            myH2 = myHeight;
+            myColor = Color.red;
+            break;
+         case 6:
+            myWidth = 30;
+            myHeight = 10;
+            myX2 = myX + 10;
+            myY2 = myY + 10;
+            myW2 = 10;
+            myH2 = 10;
+            myColor = Color.magenta;
+            break;
+         default:
+            System.out.println("ERROR AT BLOCK CLASS LINE 76 IN CONSTRUCTOR");
+            System.exit(76);
+      }
+   }
+
+   /**
+    * Accessor method for the x1 coordinate
+    * @return current x1 coordinate
+    */
+   public int getX() {
+      return myX;
+   }
+
+   /**
+    * Accessor method for the y1 coordinate
+    * @return current y1 coordinate
+    */
+   public int getY() {
+      return myY;
+   }
+
+   /**
+    * Accessor method for the color
+    * @return Current color of the block
+    */
+   public Color getColor() {
+      return myColor;
+   }
+
+   /**
+    * Modifier method for the x1 coordinate
+    * @param x desired new x1 coordinate
+    */
+   public void setX(int x) {
+      myX = x;
+   }
+
+   /**
+    * Modifier method for the y1 coordinate
+    * @param y desired new y1 coordinate
+    */
+   public void setY(int y) {
+      myY = y;
+   }
+
+   /**
+    * Modifier method for the color
+    * @param c desired new color
+    */
+   public void setColor(Color c) {
+      myColor = c;
+   }
+
+   /**
+    * Accessor method for the right acceleration
+    * @return current right acceleration
+    */
+   public boolean getRAcc(){ 
+      return racc; }
+
+   /**
+    * Accessor method for the left acceleration
+    * @return current left acceleration
+    */
+   public boolean getLAcc(){ 
+      return lacc; }
+
+   /**
+    * Modifier method for the right acceleration
+    * @param a desired right acceleration
+    */
+   public void setRAcc(boolean a){ racc = a;}
+
+   /**
+    * Modifier method for the left acceleration
+    * @param a desired left acceleration
+    */
+   public void setLAcc(boolean a){ lacc = a; }
+
+   /**
+    * Accessor method for the type
+    * @return current block type
+    */
+   public int getType(){ 
+      return myType; }
+
+   /**
+    * Accessor method for the fall variable
+    * @return current fall state
+    */
+   public static boolean getFall(){ 
+      return fall; }
+
+   /**
+    * Modifier method for the fall variable
+    * @param a desired fall starte
+    */
+   public static void setFall(boolean a){ fall = a;   }
+
+   /**
+    * Accessor method for the block width
+    * @return the current width
+    */
+   public int getWidth() {
+      if((myType == 1 && myFlipState == 0))
+         return 40;
+      else if((myType == 1 && myFlipState == 1))
+         return 10;
+      else if((myType == 2 || myType == 3 || myType == 4 || myType == 5) && (myFlipState == 0 || myFlipState == 2))
+         return 30;
+      else if((myType == 2|| myType == 3 || myType == 4 || myType == 5) && (myFlipState == 1 || myFlipState == 3))
+         return 20;
+      else if((myType == 6) && (myFlipState == 0 || myFlipState == 2))
+         return 30;
+      else if((myType == 6) && (myFlipState == 1 || myFlipState == 3))
+         return 20;
+      else
+         return 20;
+   }
+   
+   public int getHeight(){
+   
+      if((myType == 1) && (myFlipState == 0))
+         return 10;
+      else if((myType == 1) && (myFlipState ==1 ))
+         return 40;
+      else if((myType == 2 || myType == 3 || myType == 4 || myType == 5) && (myFlipState == 0 || myFlipState == 2))
+         return 20;
+      else if((myType == 2|| myType == 3 || myType == 4 || myType == 5) && (myFlipState == 1 || myFlipState == 3))
+         return 30;
+      else if((myType == 6) && (myFlipState == 0 || myFlipState == 2))
+         return 20;
+      else if((myType == 6) && (myFlipState == 1 || myFlipState == 3))
+         return 30;
+      else
+         return 20;
+      
+      
+   }
+   /**
+    * Redraws the block to adjust for any changes--reverts to original structure
+    * @param a type of block to be redrawn
+    * @deprecated does not support flipping procedure
+    */
+   public void rebuild(int a) {
+      switch (a) {
+         case 0:
+            myWidth = 20;
+            myHeight = 20;
+            myW2 = null;
+            myH2 = null;
+            break;
+         case 1:
+            myHeight = 10;
+            myWidth = 40;
+            myW2 = null;
+            myH2 = null;
+            break;
+         case 2:
+            myWidth = 30;
+            myHeight = 10;
+            myX2 = myX + 20;
+            myY2 = myY + 10;
+            myW2 = 10;
+            myH2 = 10;
+            break;
+         case 3:
+            myWidth = 30;
+            myHeight = 10;
+            myX2 = myX;
+            myY2 = myY + 10;
+            myW2 = 10;
+            myH2 = 10;
+            break;
+         case 4:
+            myWidth = 20;
+            myHeight = 10;
+            myX2 = myX + 10;
+            myY2 = myY + 10;
+            myW2 = myWidth;
+            myH2 = myHeight;
+            break;
+         case 5:
+            myWidth = 20;
+            myHeight = 20;
+            myX2 = myX + 10;
+            myY2 = myY - 10;
+            myW2 = myWidth;
+            myH2 = myHeight;
+            break;
+         case 6:
+            myWidth = 30;
+            myHeight = 10;
+            myX2 = myX + 10;
+            myY2 = myY + 10;
+            myW2 = 10;
+            myH2 = 10;
+            break;
+         default:
+            System.out.println("ERROR AT BLOCK CLASS LINE 160 IN REBUILD");
+            System.exit(160);
+      }
+   }
+
+   /**
+    * Draws the block onto a buffered image
+    * @param myBuffer desired buffered image to be drawn on
+    * @see java.awt.image.BufferedImage
+    */
+   public void draw(Graphics myBuffer){
+      myBuffer.setColor(myColor);
+      if(myType <= 1){
+         myBuffer.fillRect(myX,myY,myWidth,myHeight);
+      }
+      else{
+         myBuffer.fillRect(myX,myY,myWidth,myHeight);
+         myBuffer.fillRect(myX2, myY2, myW2, myH2);
+      }
+   }
+
+   /**
+    * Moves the block to the left or right
+    * @param x amount of spaces to be moved
+    */
+   public void move(int x){
+      if(racc && getX()+getWidth()<200){
+         myX += x;
+         myX2 = (myX2!=null) ? myX2+x : null;
+      }
+      else if(lacc && getX()>0){
+         myX -= x;
+         myX2 = (myX2 != null) ? myX2-x : null;
+      }
+   }
+
+   /**
+    * Moves the block down
+    * @param x amount of spaces to be moved
+    * @param a Direction to move
+    */
+   private void move(int x, String a) {
+      if (a.toLowerCase().equals("down")) {
+         myY += x;
+         myY2 = (myY2 != null) ? myY2 + x : null;
+      }
+      if(a.toLowerCase().equals("up")){
+         myY -= x;
+         myY2 = (myY2 != null) ? myY2 - x:null;
+      }
+   }
+
+   /**
+    * Facilitates the automatic downward movement of the blocks during the Tetris game
+    * @param blocks blocks to be rainfallen
+    * @param myBuffer BufferedImage to be drawn on
+    * @see java.awt.image.BufferedImage
+    */
+   public static void rain(ArrayList<Block> blocks, Graphics myBuffer) {
+      Block temp;
+      if(getFall()){
+         temp = blocks.get(count);
+         temp.draw(myBuffer);
+         if(temp.getY() > 400-temp.getHeight()){
+            temp.move(10, "down");
+         }
+         else{
+            constantBlocks.add(temp);
+            count++;
+         }
+         for(Block b: constantBlocks)
+            b.draw(myBuffer);
+      }
+   }
+
+   /**
+    * Rainfall animation used solely for decoration
+    * @param blocks blocks to be used
+    * @param myBuffer BufferedImage to be drawn on
+    * @see java.awt.image.BufferedImage
+    */
+   public static void prettyRain(ArrayList<Block> blocks, Graphics myBuffer) {
+      if (getFall()) {
+         Block temp;
+         for (Block b : blocks) {
+            temp = b;
+            temp.draw(myBuffer);
+            temp.move(10, "down");
+            temp.draw(myBuffer);
+         }
+      }
+   }
+
+   /**
+    * Serts a block to be in a finished state
+    * @param a block to be adjusted
+    */
+   public void setFinished(Block a){
+      Block temp = a;
+   
+   
+   }
+
+   /**
+    * Prints current type, x1, y1, width, and height of the block
+    * @return String of current state
+    */
+   public String toString(){
+      return "Type: " + myType + " X: " + myX + " Y: " + myY + " Height: " + myHeight + " Width: " + myWidth;
+   }
+
+   /**
+    * Converts the block into an <code>ArrayList</code> of <code>Rectangle2D.Double</code>
+    * @return <code>ArrayList</code> of new <code>Rectangle2D.Double</code>
+    */
+   public ArrayList<Rectangle2D.Double> convert() {
+      ArrayList<Rectangle2D.Double> arr = new ArrayList<>();
+      if (myX2 != null) {
+         arr.add(new Rectangle2D.Double(myX, myY, myWidth, myHeight));
+      } 
+      else {
+         arr.add(new Rectangle2D.Double(myX, myY, myWidth, myHeight));
+         arr.add(new Rectangle2D.Double(myX2, myY2, myW2, myH2));
+      }
+      return arr;
+   }
+
+   /**
+    * Flips the current block
+    * @param myBuffer Graphics object to be drawn onto
+    */
+   public void flip(Graphics myBuffer){
+      switch(getType()){
+         case 0: //yellow
+            break;
+         case 1: //cyan
+            int temp = myWidth;
+            myWidth = myHeight;
+            myHeight = temp;
+            if(myFlipState == 0){
+               myFlipState++;
+               
+            }
+            break;
+         default:
+            break;
+      }
+      draw(myBuffer);
+   }
+}
---
+++ leftPanel.java
@@ -0,0 +1,188 @@
+package tetris.panels;
+
+import tetris.resources.Block;
+
+import javax.swing.*;
+import javax.swing.plaf.ComponentUI;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Optional;
+
+/**
+ * A leftPanel is a panel used only for aesthetic purposes. A leftPanel holds information about its Graphical
+ * properties, along with 3 timers, an <code>ArrayList</code> of <code>Blocks</code>, and an array of possible y-positions
+ *
+ * @author Anup Bagali
+ * @author Kevin Liu
+ * @author Teja Kocherla
+ * @author Amit Rajesh
+ * @see Block
+ *
+ */
+public class leftPanel extends javax.swing.JPanel {
+    /**
+     * BufferedImage to draw onto
+     */
+    private BufferedImage myImage;
+    /**
+     * Graphics2D object which draws onto myImage
+     */
+    private Graphics2D myBuffer;
+    /**
+     * Timers to control decorations
+     */
+    private Timer t, t1, t2;
+    /**
+     * Large <code>ArrayList</code> of blocks. Used in decorations
+     */
+    private ArrayList<Block> blocks;
+    /**
+     * Used to index <code>blocks</code>
+     */
+    private int count = 0;
+    /**
+     * Used to rainfall blocks
+     */
+    private Block temp;
+    /**
+     * Stores all possible y positions for new blocks
+     */
+    private int[] yPos = new int[] {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 0};
+
+    /**
+     * Creates a new leftPanel object
+     */
+    public leftPanel(){
+        myImage = new BufferedImage(200,401, BufferedImage.TYPE_INT_RGB);
+        myBuffer = (Graphics2D) myImage.getGraphics();
+
+        blocks = new ArrayList<>();
+        blocks.add(new Block(yPos[((int) (Math.random() * 19))], 0, Optional.of(yPos[((int) (Math.random() * 19))]),
+                Optional.of(((int) (Math.random() * 401))), ((int) (Math.random() * 7))));
+
+        t = new Timer(75, new Listener());
+        t1 = new Timer(75, new Listener1());
+        t2 = new Timer(75, new Listener2());
+        t.start();
+        t1.start();
+        t2.start();
+    }
+    @Override
+    /**
+     * Calls the UI delegate's paint method, if the UI delegate
+     * is non-<code>null</code>.  We pass the delegate a copy of the
+     * <code>Graphics</code> object to protect the rest of the
+     * paint code from irrevocable changes
+     * (for example, <code>Graphics.translate</code>).
+     * <p>
+     * If you override this in a subclass you should not make permanent
+     * changes to the passed in <code>Graphics</code>. For example, you
+     * should not alter the clip <code>Rectangle</code> or modify the
+     * transform. If you need to do these operations you may find it
+     * easier to create a new <code>Graphics</code> from the passed in
+     * <code>Graphics</code> and manipulate it. Further, if you do not
+     * invoke super's implementation you must honor the opaque property, that is
+     * if this component is opaque, you must completely fill in the background
+     * in an opaque color. If you do not honor the opaque property you
+     * will likely see visual artifacts.
+     * <p>
+     * The passed in <code>Graphics</code> object might
+     * have a transform other than the identify transform
+     * installed on it.  In this case, you might get
+     * unexpected results if you cumulatively apply
+     * another transform.
+     *
+     * @param g the <code>Graphics</code> object to protect
+     * @see #paint
+     * @see ComponentUI
+     */
+    public void paintComponent(Graphics g){
+        g.drawImage(myImage, 0, 0, getWidth(), getHeight(), null);
+    }
+
+    /**
+     * Shows the decorations onto the panel
+     */
+    public void decorate(){
+        temp = new Block(yPos[((int) (Math.random() * 19))], 0, Optional.of(yPos[((int) (Math.random() * 19))]),
+                Optional.of(((int) (Math.random() * 401))), ((int) (Math.random() * 7)));
+
+        blocks.add(temp);
+
+        myBuffer.setColor(Color.black);
+        myBuffer.fillRect(0, 0, getWidth(), getHeight());
+        myBuffer.setColor(Color.WHITE);
+
+        blocks.get(count).draw(myBuffer);
+        blocks.get(count).move(10);
+
+        Block.setFall(true);
+        Block.prettyRain(blocks, myBuffer);
+
+        if (blocks.get(count).getY() == ((blocks.get(count).getType() != 1) ? 380 : 390))
+            count++;
+
+        repaint();
+        revalidate();
+    }
+
+    /**
+     * A nested class for decorations
+     * @see ActionListener
+     */
+    private class Listener implements ActionListener {
+        /**
+         * Invoked when an action occurs.
+         *
+         * @param e the event to be processed
+         */
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            decorate();
+        }
+    }
+
+    /**
+     * A nested class for more decorations
+     * @see ActionListener
+     */
+    private class Listener1 implements ActionListener {
+        /**
+         * Invoked when an action occurs.
+         *
+         * @param e the event to be processed
+         */
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            decorate();
+        }
+    }
+
+    /**
+     * A nested for even more decorations
+     * @see ActionListener
+     */
+    private class Listener2 implements ActionListener {
+        /**
+         * Invoked when an action occurs.
+         *
+         * @param e the event to be processed
+         */
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            decorate();
+        }
+    }
+
+    /**
+     * Stops all timers
+     */
+    public void stop(){
+        t.stop();
+        t1.stop();
+        t2.stop();
+    }
+}
