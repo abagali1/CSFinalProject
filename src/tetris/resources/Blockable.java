4f4629b327da03b4c42b488bd2c706657c96df89
abagali1 <a.bagali815703@gmail.com>
2018-05-21 11:37:39 -0400

    Prep for big changes

--- src/tetris/resources/Blockable.java
+++ src/tetris/resources/Blockable.java
@@ -3,7 +3,7 @@ package tetris.resources;
 import java.awt.*;
 
 /**
- * The Blockable interface defines an object as a Block. It defines the method flip()
+ * The Blockable interface defines an object as a Block. It defines the method flip() and move(int x)
  */
 public interface Blockable {
     /**
@@ -11,4 +11,10 @@ public interface Blockable {
      * @param myBuffer Graphics object to be drawn onto
      */
     void flip(Graphics myBuffer);
+    /**
+     * Moves the block to the left or right
+     * @param x amount of spaces to be moved
+     */
+    void move(int x);
+
 }
